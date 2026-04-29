ef80e420e6f8f8efa14796d6b78acd25d02f3faa
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-29 17:42:23 +0300

    Task: 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,8 @@
  *binary_tree_node - it is a function that creat a new node
  *@parent : is a pointer to the parent node
  *@value : value is the value to put in the new node
- *Return : return a pointer to the new node, or NULL on failure
+ *
+ *Return: return a pointer to the new node, or NULL on failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
