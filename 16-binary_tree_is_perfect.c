761bdecb2128b8056a8cfcacf75f574df514b6bd
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-06 18:58:01 +0000

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,7 +6,7 @@
  *
  * Return: Height of the tree if perfect, -1 if not perfect
  */
-int tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree);
 {
 if (tree == NULL)
 	return (0);
@@ -14,10 +14,9 @@ if (tree->left == NULL && tree->right == NULL)
 {
 	return (1);
 }
-if (binary_tree_is_full(tree) && 
-		binary_tree_height(tree->left) == binary_tree_height(tree->right))
+if (binary_tree_is_full(tree) && binary_tree_height(tree->left) == binary_tree_height(tree->right))
 {
-return (tree_is_perfect(tree->left) && tree_is_perfect(tree->right));
+return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
 return (0);
 }
