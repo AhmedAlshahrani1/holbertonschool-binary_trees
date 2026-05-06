79e27c9f284c2f378824b10d8d5488b48a45372e
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-06 20:27:39 +0000

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,8 +10,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *grandparent;
 if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-	grandparent = node->parent->parent;
 	return (NULL);
+ grandparent = node->parent->parent;
 if (grandparent->left == node->parent)
 {
 	return (grandparent->right);
