e03785201143a1d30eb71e66887497a57f3ea158
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-06 17:27:24 +0000

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,27 +8,26 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height = 0;
-        size_t right_height = 0;
-
-        if (tree == NULL)
-        {
-                return (0);
-        }
-        if (tree->left != NULL)
-        {
-                left_height = binary_tree_height(tree->left) + 1;
-        }
-        if (tree->right != NULL)
-        {
-                right_height = binary_tree_height(tree->right) + 1;
-        }
-        if (left_height >= right_height)
-        {
-                return (left_height);
-        }
-        else
-                return (right_height);
+size_t left_height = 0;
+size_t right_height = 0;
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left != NULL)
+{
+left_height = binary_tree_height(tree->left) + 1;
+}
+if (tree->right != NULL)
+{
+right_height = binary_tree_height(tree->right) + 1;
+}
+if (left_height >= right_height)
+{
+return (left_height);
+}
+else
+return (right_height);
 }
 int binary_tree_balance(const binary_tree_t *tree)
 {
