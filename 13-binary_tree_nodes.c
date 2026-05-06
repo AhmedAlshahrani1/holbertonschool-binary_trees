04646c8fd5862c7dd153f6c5ebc90f99b56ccba8
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-06 16:38:02 +0000

    Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,16 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t count = 0;
 if (tree == NULL)
 {
 return (0);
 }
 if (tree->left != NULL || tree->right != NULL)
 {
-count++;
-binary_tree_nodes(tree->left);
-binary_tree_nodes(tree->right);
+return(1+binary_tree_nodes(tree->left)+binary_tree_nodes(tree->right));
 }
-return (count);
+return (0);
 }
