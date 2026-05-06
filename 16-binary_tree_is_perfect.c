d195841b1d114f19ee0bbb980b2a79f703bc89d4
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-06 18:55:18 +0000

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,8 @@ if (tree->left == NULL && tree->right == NULL)
 {
 	return (1);
 }
-if (binary_tree_is_full(tree) && binary_tree_height(tree->left) == binary_tree_height(tree->right))
+if (binary_tree_is_full(tree) && 
+		binary_tree_height(tree->left) == binary_tree_height(tree->right))
 {
 return (tree_is_perfect(tree->left) && tree_is_perfect(tree->right));
 }
